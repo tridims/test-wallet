6f3556bf29ec0a6b6520fbd92c742e8f265bb466
Dimas Tri Mustakim <test31122001@gmail.com>
2023-10-27 18:33:18 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,23 @@ const URL = 'https://sepolia.infura.io/v3/7746850a8fb348599a052c1c18d78a14';
 const PASSWORD = 'password';
 
 async function main() {
-  const wallet = await MyWallet.loadFromFile(PASSWORD);
+  // const wallet = new MyWallet('testing password');
+  const wallet = await MyWallet.loadFromFile('testing password');
+  // const wallet = await MyWallet.loadFromFile(PASSWORD, 'my-wallet-old.json');
+
   const provider = new ethers.JsonRpcProvider(URL);
   await wallet.connectProvider(provider);
 
   // BALANCE
-  // await wallet.showAccountsBalance();
+  await wallet.showAccountsBalance();
+
+  // const account1 = wallet.accountsAt(0);
+  // const account2 = wallet.accountsAt(2);
+
+  // ACCOUNT INFO
+  // console.log(account1);
 
   // TRANSFER
-  // const account1 = wallet.accountsAt(0);
-  // const account2 = wallet.accountsAt(1);
-
   // const tx = await account1.sendTransaction({
   //   to: account2.address,
   //   value: ethers.parseEther('0.00001')
@@ -25,8 +31,8 @@ async function main() {
   // console.log(tx);
 
   // CREATE ACCOUNT
-  const newAccount = await wallet.createNewAccount();
-  console.log(newAccount);
+  // const newAccount = await wallet.createNewAccount();
+  // console.log(newAccount);
 }
 
 main();
